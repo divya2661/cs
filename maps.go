c9ebd06e180963c9b5e286c53eb90ee61a79dfe5
YOUR NAME <divn2661@gmail.com>
2014-08-07 17:03:55 +0530

    maps

---
+++ maps.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+    Lat, Long float64
+}
+
+
+func main() {
+	
+	m := make(map[string]int)
+
+	m["answer"] = 42
+	fmt.Println("the value: ", m["answer"])
+
+	m["answer"] = 48
+	fmt.Println("the value: ", m["answer"])
+
+	delete(m,"answer")
+	fmt.Println("the value: ", m["answer"])
+
+	v, ok := m["answer"]
+
+    fmt.Println("the value: ", v, "present?", ok)
+}
